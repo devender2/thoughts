06737536079d8229e80401fc56fb4721cfef756a
Brandon Barringer <barringerbrandon@outlook.com>
2014-08-20 07:30:50 -0700

    created wrapper div around pagination

--- thoughts/functions.php
+++ thoughts/functions.php
@@ -29,18 +29,22 @@ function thoughts_paginate() {
      
     $total_pages = $wp_query->max_num_pages;
      
-    if ($total_pages > 0){
+    if ($total_pages > 1){
      
-      $current_page = max(1, get_query_var('paged'));
-       
-      echo paginate_links(array(
-          'base' => get_pagenum_link(1) . '%_%',
-          'format' => '/page/%#%',
-          'current' => $current_page,
-          'total' => $total_pages,
-          'prev_text' => 'Prev',
-          'next_text' => 'Next'
+        $current_page = max(1, get_query_var('paged'));
+        
+        echo '<div class="paginate">'
+
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+            'prev_text' => 'Prev',
+            'next_text' => 'Next'
         ));
+
+        echo '</div>'
     }
 }
 endif; 
